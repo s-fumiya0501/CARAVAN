d732928bd17bc1bd44d67133840704619c731d77
EC2 Default User <ec2-user@ip-172-31-74-52.ec2.internal>
2021-07-26 13:21:44 +0000

    main-first

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,24 @@
+class BlogsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def edit
+    
+  end
+  
+  def create
+    
+  end
+  
+  
+  
+end
